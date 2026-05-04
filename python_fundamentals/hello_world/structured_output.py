3922aacc391eb89183dea17ef60d508778495c3c
22niko-spw <nicosojd@gmail.com>
2026-05-04 13:07:25 +0200

    fix: add boolean and formatted string interpolation

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+pi = 3.14159
+
 print("Language: Python")
 print("Version: 3")
-print("Pi approx: 3.14")
-print("Computation valid: True")
+print("Pi approx: %.2f" % pi)
+print("Computation valid:", 3.14 == 3.14)
