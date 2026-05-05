c67683e7d0581ee3e689308e95a2c2fdd4c340bb
22niko-spw <nicosojd@gmail.com>
2026-05-05 09:46:18 +0200

    fix: stock char(i) in char

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 for i in range(97, 123):
+    char = chr(i)
+
     if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print(chr(i), end='')
+    print(format(char), end='')
