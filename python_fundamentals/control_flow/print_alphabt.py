533f3f8c1be2b5743091a40df55d4add5f41f4bb
22niko-spw <nicosojd@gmail.com>
2026-05-05 09:51:03 +0200

    fix: add .format() in printf

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 for i in range(97, 123):
-    char = chr(i)
 
     if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print(format(char), end='')
+    print("{}".format(chr(i)), end='')
