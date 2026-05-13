7b3d549711e68cc569efc86a96a08392b66ac7dd
22niko-spw <nicosojd@gmail.com>
2026-05-13 15:43:13 +0200

    Add: Define a class named Square that represents a square.

---
+++ python_oop/classes_and_object_model/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Square module - defines a basic Square class
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    This is a basic version with no attributes or methods.
+    """
+    pass
