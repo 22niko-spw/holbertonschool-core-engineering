e4af79f0d42100c0788e50b78a63605096003820
22niko-spw <nicosojd@gmail.com>
2026-05-14 13:39:10 +0200

    Add:dd getters and setters for the size attribute.

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Square module - defines a Square class with size attribute
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square (private)
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the square's side
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
