02ffbd9a3b7cfdf1c5d775f6f2987bfc736cc43b
22niko-spw <nicosojd@gmail.com>
2026-05-14 12:57:00 +0200

    Fix: convert tab in spaces

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -19,8 +19,8 @@ class Square:
         Args:
             size (int): The size of the square's side
         """
-		if not isinstance(size, int):
-			raise TypeError("size must be an integer")
-		if size < 0
-			raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0
+            raise ValueError("size must be >= 0")
         self.__size = size
