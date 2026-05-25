41c121b84a58cf3348bd72c35bedf97bbb231899
22niko-spw <nicosojd@gmail.com>
2026-05-25 13:32:39 +0200

    Fix : add comment

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+"""Module for reading file"""
 
 def read_file(filename=""):
-	"""read a file"""
+    """read a file"""
 
-	with open("fichier.txt", "r", encoding="utf-8") as f:
-		for line in f:
-			print(line, end="")
+    with open("fichier.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
