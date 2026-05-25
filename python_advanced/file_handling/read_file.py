e428d9fa6a50a9a7014e102a7783a685ce4827fc
22niko-spw <nicosojd@gmail.com>
2026-05-25 13:34:07 +0200

    Fix : change the filename

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -4,6 +4,6 @@
 def read_file(filename=""):
     """read a file"""
 
-    with open("fichier.txt", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
